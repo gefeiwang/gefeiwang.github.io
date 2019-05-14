9a30913ed7d1928392b2f9987e46fd82ff861d8f
gefeiwang <wanggefei1997@163.com>
2019-05-14 21:45:56 +0800

    update

--- img/cut.py
+++ img/cut.py
@@ -2,7 +2,7 @@ import imageio
 import numpy as np
 import PIL.Image
 
-img = np.asarray(PIL.Image.open('700x700.png').resize((195,195)))
+img = np.asarray(PIL.Image.open('700x700.png').resize((700,700)))
 
 # img = np.pad(img, [(64,64), (64+32,64+32), (0,0)], 'constant', constant_values=0)
 
